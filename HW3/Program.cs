292236a15c4cbf7d072ef4539dcacc0ab94e1029
Zokerzes <romzes85@mail.ru>
2022-08-31 20:23:01 +0300

    _task6 marker_produkt

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -257,38 +257,56 @@ namespace hw3
     }
     
     
-    class Task5
+   
+    class Task5_market
     {
-        /**/
+        /*Создайте класс «Магазин». Необходимо хранить в 
+        полях класса: название магазина, адрес, описание профиля магазина, контактный телефон, контактный e-mail. 
+        Реализуйте методы класса для ввода данных, вывода 
+        данных, реализуйте доступ к отдельным полям через 
+        методы класса. продать товар, принять товар
+        класс товар имя, цена*/
+        string name;
+        string path;
+        string descrip;
+        string tel;
+        string e-mail;
+        double balance=0;
+        
+        //вложеный класс товар
+        class Product 
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public double price { get; set; }
+
+            Product() 
+            {
+                this.Name = "prod0";
+                this.price = 0.0;
+                this.Description = "something virtual values";
+            }
+        }
+
+        Product []product= new Product[1];
+
+        public void acceptProduct() 
+        {
+
+        }
+
         public void StartTask5()
-
         {
 
         }
     }
-    class Task6
-    {
-        /*.*/
-        public void StartTask6()
-        {
-
-        }
-    }
-    class Task7
-    {
-        /*
-		*/
-        public void StartTask7()
-        {
-
-        }
-    }
+    
     class HomeWork
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("1 - задача 1\n2 - задача 2\n3 - задача 3\n" +
-                "4 - задача 4\n5 - задача 5\n6 - задача 6\n7 - задача 7");
+                "4 - задача 4\n5 - задача 5");
             Int32 key = Convert.ToInt32(Console.ReadLine());
             switch (key)
             {
@@ -312,15 +330,7 @@ namespace hw3
                     Task5 task5 = new Task5();
                     task5.StartTask5();
                     break;
-                case 6:
-                    Task6 task6 = new Task6();
-                    task6.StartTask6();
-                    break;
-                case 7:
-                    Task7 task7 = new Task7();
-                    task7.StartTask7();
-                    break;
-
+                
                 default:
                     break;
 
